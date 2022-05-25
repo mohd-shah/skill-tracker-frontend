e75b301c8b9ae28633a8d76bf8956891f47611c8
Mohd Shahid <mohd.shahid2207@gmail.com>
2022-05-25 12:44:55 +0530

    AWS API gateway support

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const config = {
-    APIURL : "http://ec2-13-233-96-16.ap-south-1.compute.amazonaws.com:8091/skill-tracker/api/v1/admin"
+    APIURL : "https://hx17egy27g.execute-api.ap-south-1.amazonaws.com/prod/admin-service"
 }
